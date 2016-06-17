a99da33b623edd616882a0ebed039c347e7932fe
Eric A Mullen <emullen@cs.washington.edu>
2016-06-17 10:50:27 -0700

    some tests for break_if

--- Tests.v
+++ Tests.v
@@ -37,13 +37,13 @@ Proof.
          end.
   (* make sure no more hypotheses *)
   match goal with
-  | [ H : _ = _ |- _ ] => fail "subst_max didn't substitute everything"
+  | [ H : _ = _ |- _ ] => fail 2 "subst_max didn't substitute everything"
   | [ |- _ ] => idtac
   end.
   (* make sure goal solvable by reflexivity *)
   match goal with
   | [ |- ?X = ?X ] => reflexivity
-  | _ => fail "subst_max didn't fully substitute in goal"
+  | _ => fail 2 "subst_max didn't fully substitute in goal"
   end.
 Qed.
 
@@ -56,3 +56,81 @@ Qed.
 (* break_if *)
 (* Desired properties: *)
 (* - does in fact break apart if statements *)
+Lemma break_if_test_bool_1 :
+  forall (x : bool) (A : Type) (a b y : A),
+         (y = if x then a else b) ->
+         y = a \/ y = b.
+Proof.
+  intros.
+  break_if;
+    firstorder.
+Qed.
+
+(* break_if *)
+(* make sure gets many different if expressions *)
+Lemma break_if_test_many :
+  forall (x1 x2 x3 x4 x5 : bool) (A : Type) (a b y : A),
+         (y = if x1 then a else b) ->
+         (y = if x2 then a else b) ->
+         (y = if x3 then a else b) ->
+         (y = if x4 then a else b) ->
+         (y = if x5 then a else b) ->
+         y = a \/ y = b.
+Proof.
+  intros.
+  repeat break_if;
+    (* make sure no more if exprs in hypotheses *)
+    match goal with
+    | [ H : context[if _ then _ else _ ] |- _] => fail 2 "still if exprs in hypotheses"
+    | [ |- _ ] => idtac
+    end;
+    try solve [left; auto].
+  right. auto.
+Qed.
+
+Inductive threecon :=
+| A
+| B
+| C.
+
+(* break_if doesn't get other matches *)
+Lemma break_if_no_destruct_match :
+  forall (n : threecon) (A : Type) (a b c x : A),
+    (x = match n with
+        | A => a
+        | B => b
+        | C => c
+         end) ->
+    x = a \/ x = b \/ x = c.
+Proof.
+  intros.
+  repeat break_if;
+    match goal with
+    | [ H : context[match _ with _ => _ end] |- _ ] => idtac
+    | [ |- _ ] => fail 2 "break_if destructed the match"
+    end.
+Abort. (* we make sure that we don't break apart the match, don't need to prove the goal *)
+
+Lemma break_if_destructs_goal :
+  forall (x : bool) (A : Type) (a b y : A),
+    a = b ->
+    y = a ->
+    (y = if x then a else b).
+Proof.
+  intros.
+  break_if;
+  match goal with
+  | [ |- context[if _ then _ else _]] => fail 2 "break if didn't destruct goal"
+  | [ |- _ ] => idtac
+  end;
+  congruence.
+
+Qed.
+
+
+
+  
+  
+
+
+
