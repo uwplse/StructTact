566dbbc40280b23b1a920472e59201ffea8e4f8b
Karl Palmskog <palmskog@gmail.com>
2017-01-15 13:18:28 -0600

    use opam-dev repo in travis

--- .travis-ci.sh
+++ .travis-ci.sh
@@ -4,14 +4,13 @@ opam init --yes --no-setup
 eval $(opam config env)
 
 opam repo add coq-released https://coq.inria.fr/opam/released
-opam repo add distributedcomponents http://opam.distributedcomponents.net
-
 opam pin add coq $COQ_VERSION --yes --verbose
 
 ./build.sh
 
 case $DOWNSTREAM in
 verdi-raft)
+  opam repo add distributedcomponents-dev http://opam-dev.distributedcomponents.net
   opam install coq-mathcomp-ssreflect.$SSREFLECT_VERSION InfSeqExt verdi-runtime --yes --verbose
   pushd ..
     git clone 'https://github.com/uwplse/verdi.git'
